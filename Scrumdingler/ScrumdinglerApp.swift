b6ad5cef9820473f197693de577b5c5b55eca9c6
MoonGoon <moongoon.cnu@gmail.com>
2023-01-26 14:08:24 +0900

    Initial Commit

---
+++ Scrumdingler/ScrumdinglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdinglerApp.swift
+//  Scrumdingler
+//
+//  Created by 문영균 on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdinglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
