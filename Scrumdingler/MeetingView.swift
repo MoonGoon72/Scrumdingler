a38792f785c6431febd7827617c3c9b9f4f8bdc5
MoonGoon <moongoon.cnu@gmail.com>
2023-01-28 14:26:22 +0900

    Clear section 2

--- Scrumdingler/MeetingView.swift
+++ Scrumdingler/MeetingView.swift
@@ -12,8 +12,14 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                Text("Seconds Elapsed")
-                Text("Seconds Elapsed")
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
             }
         }
     }
