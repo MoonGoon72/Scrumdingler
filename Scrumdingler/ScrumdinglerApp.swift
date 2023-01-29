ac0cf8af974f5bdbf93ded27304b89242d1f7b6a
MoonGoon <moongoon.cnu@gmail.com>
2023-01-29 15:54:27 +0900

    Clear: Displaying data in a list, Section 2 / 2

--- Scrumdingler/ScrumdinglerApp.swift
+++ Scrumdingler/ScrumdinglerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScrumdinglerApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }
