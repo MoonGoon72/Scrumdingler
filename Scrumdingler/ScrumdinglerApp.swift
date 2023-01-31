41595389292d44668c4e9a87019da5d23ccac057
MoonGoon <moongoon.cnu@gmail.com>
2023-01-31 20:32:52 +0900

    Update: Creating a navigation hierarchy section 1 / 5

--- Scrumdingler/ScrumdinglerApp.swift
+++ Scrumdingler/ScrumdinglerApp.swift
@@ -1,17 +1,12 @@
-//
-//  ScrumdinglerApp.swift
-//  Scrumdingler
-//
-//  Created by 문영균 on 2023/01/26.
-//
-
 import SwiftUI
 
 @main
 struct ScrumdinglerApp: App {
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: DailyScrum.sampleData)
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
         }
     }
 }
